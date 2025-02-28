8c91a2a7e7cf254337855abb2f99e96be92c4c12
beki-kel <bereket.kelay@aait.edu.et>
2025-02-28 14:40:44 +0300

    Add id attribute to search form for improved accessibility and targeting

--- Online-Booking-Management/src/app/(server-components)/SectionHero.tsx
+++ Online-Booking-Management/src/app/(server-components)/SectionHero.tsx
@@ -28,7 +28,10 @@ const SectionHero: FC<SectionHeroProps> = ({ className = "" }) => {
         </div>
       </div>
 
-      <div className="hidden lg:block z-10 mb-12 lg:mb-0 lg:-mt-40 w-full">
+      <div
+        id="searchForm"
+        className="scroll-mt-24 hidden lg:block z-10 mb-12 lg:mb-0 lg:-mt-40 w-full"
+      >
         <HeroSearchForm />
       </div>
     </div>
