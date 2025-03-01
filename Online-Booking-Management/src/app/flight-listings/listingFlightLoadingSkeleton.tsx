6479be5309ee1432de0ac9b1667365de67345a03
beki-kel <bereket.kelay@aait.edu.et>
2025-03-01 13:01:51 +0300

    Update flight loading skeleton height for improved visual consistency

--- Online-Booking-Management/src/app/flight-listings/listingFlightLoadingSkeleton.tsx
+++ Online-Booking-Management/src/app/flight-listings/listingFlightLoadingSkeleton.tsx
@@ -9,7 +9,7 @@ const listingFlightLoadingSkeleton: FC<listingFlightLoadingSkeletonProps> = ({
 }) => {
   return (
     <div role="status" className="animate-pulse w-full">
-      <div className=" bg-gray-200 dark:bg-gray-700  h-28 mb-8 "></div>
+      <div className=" bg-gray-200 dark:bg-gray-700  h-24 mb-8 "></div>
       <div className="h-10 bg-gray-200 rounded-full dark:bg-gray-700  mb-4"></div>
       <div className="lg:p-10 grid grid-cols-1 gap-6 bg-gray-200 rounded-3xl mb-20">
         <div className="h-20 bg-gray-100 rounded-lg  dark:bg-gray-700 "></div>
