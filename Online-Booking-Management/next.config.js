faa23edd7eaa426fc56d5584a449cbddfef1f6c3
beki-kel <bereket.kelay@aait.edu.et>
2025-03-01 13:58:47 +0300

    Update Next.js configuration and adjust package dependencies

--- Online-Booking-Management/next.config.js
+++ Online-Booking-Management/next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  experimental: {
+    appDir: true,
+    typedRoutes: true,
+  },
   images: {
     remotePatterns: [
       {
